609b624800d3294c25214198157b3568718bf731
Jean-Lionel <nijeanlionel@gmail.com>
2023-04-29 04:41:46 +0200

    instal bootsrapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: "app-root",
     template: `<h1>{{title}}</h1>`,
-    styleUrls : [],
+    styleUrls : ["./app.component.css"],
 
 })
 export class AppComponent{
