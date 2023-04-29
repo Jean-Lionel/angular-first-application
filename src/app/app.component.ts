143978a8a8c507d7304c92651f66deaeb4c24ed4
Jean-Lionel <nijeanlionel@gmail.com>
2023-04-29 05:41:45 +0200

    angular condition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "app-root",
-    template: `<h1>{{title}}</h1>`,
+    templateUrl: "./app.component.html",
     styleUrls : ["./app.component.css"],
 
 })
 export class AppComponent{
-    title: string = "Je suis un Millionnaire";
+    title: string = "Gestionnaire Hotel";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppHotelListComponent } from './hotel-list/hotel-list';
 
 @NgModule({
   declarations: [
-    AppComponent
+        AppComponent,
+      AppHotelListComponent,
   ],
   imports: [
     BrowserModule
