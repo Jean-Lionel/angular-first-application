c91092205f9a36cf2e5d11429337161c54d55e96
Jean-Lionel <nijeanlionel@gmail.com>
2023-05-02 05:46:26 +0200

    adding start rating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppHotelListComponent } from './hotel-list/hotel-list';
 import { FormsModule } from '@angular/forms';
 import { ReplaceComma } from './shared/pipes/replace-commant';
+import { StartRating } from './shared/start-rating/start-rating';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHotelListComponent,
     ReplaceComma,
+    StartRating,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/start-rating/start-rating.ts
@@ -0,0 +1,14 @@
+import { Component, OnChanges, SimpleChanges } from "@angular/core";
+
+@Component({
+  selector : 'app-start-rating',
+  templateUrl: "./start-rating.html",
+  styleUrls : ["./start-rating.css"],
+})
+export class StartRating implements OnChanges{
+
+  ngOnChanges(): void {
+
+  }
+
+}
