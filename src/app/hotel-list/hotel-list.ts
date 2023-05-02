805ca80e4c1b5e204212a3ee6d6f582317536cb2
Jean-Lionel <nijeanlionel@gmail.com>
2023-05-02 06:38:38 +0200

    les services

--- src/app/hotel-list/hotel-list.ts
+++ src/app/hotel-list/hotel-list.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IHotel } from "./hotel";
+import { HotelListeService } from "./hotel-liste.services";
 
 @Component({
     selector: 'app-hotel-list',
@@ -15,8 +16,14 @@ export class AppHotelListComponent implements OnInit {
   public hotelFilter: string = '';
   public _filtedList: IHotel[] = [];
   public messageRating: string | undefined;
+  public hotels: IHotel[] = [];
 
-   ngOnInit(): void {
+  constructor(private hotelList: HotelListeService) {
+
+  }
+
+  ngOnInit(): void {
+     this.hotels = this.hotelList.getHotel();
      this._filtedList = this.hotels;
   }
 
@@ -33,38 +40,6 @@ export class AppHotelListComponent implements OnInit {
     this.messageRating = message;
   }
 
-  public hotels: IHotel[] = [
-    {
-      hotelId: 1,
-      hotelName: "Buea sweet life",
-      description: "Belle vue au bord de la mer",
-      price: 230.5,
-      imageUrl: "assets/img/hotel-room.jpg",
-      rating: 3.5
-    },
-    {
-      "hotelId": 2,
-      "hotelName": "Marakech",
-      "description": "Profitez de la vue sur les montagnes",
-      "price": 145.5,
-      "imageUrl": "assets/img/the-interior.jpg",
-      "rating": 5
-    },
-    {
-      "hotelId": 3,
-      "hotelName": "Abudja new look palace",
-      "description": "Séjour complet avec service de voitures",
-      "price": 120.12,
-      "imageUrl": "assets/img/indoors.jpg",
-      "rating": 4
-    },
-    {
-      "hotelId": 4,
-      "hotelName": "Cape town city",
-      "description": "Magnifique cadre pour votre séjour",
-      "price": 135.12,
-      "imageUrl": "assets/img/window.jpg",
-      "rating": 2.5
-    },
-  ];
+
+
 }
